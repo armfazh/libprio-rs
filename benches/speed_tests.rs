19e3b8ecfe888b384967145f29fc3ef40977c78e
armfazh <armfazh@cloudflare.com>
2022-02-14 16:28:31 -0800

    Benchmarking with hash to curve.

--- benches/speed_tests.rs
+++ benches/speed_tests.rs
@@ -2,7 +2,7 @@
 
 use criterion::{criterion_group, criterion_main, Criterion};
 
-use prio::field::{random_vector, random_vector2, Field128};
+use prio::field::{random_vector, random_vector2, random_vector3, Field128};
 
 // Benchmark for random_vector using rejection sampling
 pub fn prng(c: &mut Criterion) {
@@ -24,8 +24,18 @@ pub fn prng2(c: &mut Criterion) {
     }
 }
 
+// Benchmark for random_vector using hash_to_curve and
+pub fn prng3(c: &mut Criterion) {
+    let test_sizes = [16, 256, 1024, 4096];
+    for size in test_sizes.iter() {
+        c.bench_function(&format!("hash_to_curve, size={}", *size), |b| {
+            b.iter(|| random_vector3::<Field128>(*size))
+        });
+    }
+}
+
 // TODO Phillipp's method.
 
 // TODO Add benchmarks for different prng methods for prio3.
-criterion_group!(benches, prng, prng2);
+criterion_group!(benches, prng, prng2, prng3);
 criterion_main!(benches);
